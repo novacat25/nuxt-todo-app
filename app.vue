c368ea3aac0392a8fb88cb506c399245a6cf87bc
NovaCat <sparkstorms2@gmail.com>
2024-05-03 16:22:15 +0900

    Fix: placeholder

--- app.vue
+++ app.vue
@@ -43,7 +43,7 @@ const people = [{
       </template>
       <div class="flex justify-between pb-8 gap-2">
         <div class="w-full">
-          <UInput v-model="value" placeholder="Write your statuss" size="lg" />
+          <UInput v-model="value" placeholder="Write your status" size="lg" />
         </div>
         <UButton>Submit</UButton>
       </div>

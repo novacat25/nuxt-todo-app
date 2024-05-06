5babb470fd0f10c211cab14dc585b305a1770051
NovaCat <sparkstorms2@gmail.com>
2024-05-06 17:42:41 +0900

    Design: add px on InputTask.vue

--- components/InputTask.vue
+++ components/InputTask.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="flex justify-between pb-8 gap-2">
+    <div class="flex justify-between pb-8 gap-2 px-5">
         <div class="w-full">
           <UInput v-model="value" placeholder="Write your tasks" size="lg" />
         </div>
